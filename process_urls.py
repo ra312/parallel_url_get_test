c6420112a918e8351cafd7ee559aacfa180005f6
ra312 <akylzhanov.r@gmail.com>
2020-05-21 18:57:51 +0600

    added parallel call count

--- process_urls.py
+++ process_urls.py
@@ -7,22 +7,20 @@ Returns:
 # some urls return 403 while I can access the server by hand,
 			# therefore I include user-agent in the header
 			# 'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
-
-import multiprocessing
-import requests
 import sys
-
-
+# global_id = dict()
+global_id = dict()
 import requests
 # all subexceptions inherit from the base class below
-from requests import ConnectionError
+
 
 import multiprocessing
 number_of_cpus = multiprocessing.cpu_count()
 print(f"number_of_cpus={number_of_cpus}")
+
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 header = {'User-Agent': user_agent}
-timeout=1.0 # what is the time unit here ?
+timeout=10.0 # what is the time unit here ?
 pool = multiprocessing.Pool(processes=number_of_cpus)
 urls_file = "list_of_urls"
 
@@ -35,18 +33,30 @@ def get_all_urls(file=urls_file):
 
 
 
-def main():
-	urls = get_all_urls(file=urls_file)
+def process_urls(urls):
 	try:
 		pool_outputs = pool.map(get_stuff, urls)	
-	except ConnectionError as error:
+	except requests.ConnectionError as error:
 		print(error)
-		pass
 	except KeyboardInterrupt as keyboard_exc:
 		print(keyboard_exc)
-		pass
 	pool.close()
-	print(pool_outputs)
-			
+	return pool_outputs
+
+def separate_stats_from_output(outputs):
+	call_stats = dict((id_p,0) for id_p in range(1,number_of_cpus+1,1))
+	pool_outputs = []
+	for tuple in outputs:
+		id_p = tuple[0]
+		call_stats[id_p] += 1
+		pool_outputs.append(tuple[1])
+	return call_stats, '\n'.join(pool_outputs)
+def print_call_stats(call_stats):
+	for id_p, called_times  in call_stats.items():
+		print(f"{id_p}:{called_times}")
 if __name__ == '__main__':
-	main()
+	urls = get_all_urls(file=urls_file)
+	outputs = process_urls(urls)
+	call_stats, pool_outputs = separate_stats_from_output(outputs)
+	print(pool_outputs)
+	print_call_stats(call_stats)
